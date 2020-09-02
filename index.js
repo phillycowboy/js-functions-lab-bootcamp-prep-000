de89fefd36793406f1151c49a892621c497392d9
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-02 18:30:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
 
-var holiday = "Independence Day",
-    name = "Ben";
 
-    function happyCustomHolidayTo(holiday, name){
-      return `Happy ${holiday},${name}!`;
-    }
+function happyCustomHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+}
